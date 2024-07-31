bf86de1b89014a8f5e1c856b9ea95520c80c6593
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-07-31 22:22:31 +0500

    fifth

---
+++ react-app-3/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+
+function App() {
+ const [color, setColor] = useState("white")
+
+  return (
+    <div className="w-full h-screen duration-500" style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-0">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-black px-3 py-2 rounded-3xl">
+          <button 
+          onClick={() => setColor("magenta")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "red"}}>Red</button>
+          <button 
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "pink"}}>Pink</button>
+          <button 
+          onClick={() => setColor("purple")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "purple"}}>Purple</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
